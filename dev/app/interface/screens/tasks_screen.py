625302db376e6777450ae1d891133d0c4151bc5f
gabriel Linux <carmogabriel0429@gmail.com>
2025-11-21 23:29:48 -0300

    correção na inserção de tarefas na tela de tarefas

--- dev/app/interface/screens/tasks_screen.py
+++ dev/app/interface/screens/tasks_screen.py
@@ -1,7 +1,10 @@
+from kivy.lang.builder import Builder
 from kivy.uix.settings import text_type
 from kivy.uix.screenmanager import Screen
 from app.interface.widgets.modalTask_iten import ModalTask
 from app.interface.widgets.task_iten import Task
+
+Builder.load_file('dev/app/interface/kvLang/TasksPage.kv')
 
 class TasksPage(Screen):
     def insertModal(self):
@@ -10,14 +13,14 @@ class TasksPage(Screen):
         self.modal.open()
         
     def insertTask(self):
-        self.modal.parent_screen = self
+        self.modal.parent_screen = self  # tirar
         titulo= self.modal.ids.inputTitle.text
         prazo= self.modal.ids.inputDate.text
         responsavel= self.modal.ids.inputPerson.text
         descricao=self.modal.ids.inputDescription.text
 
         self.ids.containerTasks.add_widget(
-            Task(titulo,prazo,responsavel,descricao)
+            Task(titulo, prazo, responsavel, descricao) # reordenar
         )
 
     
--- dev/app/interface/widgets/task_iten.py
+++ dev/app/interface/widgets/task_iten.py
@@ -1,13 +1,22 @@
 from kivy.lang.builder import Builder
 from kivy.uix.boxlayout import BoxLayout
 
-Builder.load_file('dev/app/interface/kvLang/Task.kv')
+Builder.load_file('dev/app/interface/kvLang/task.kv')
 
 class Task(BoxLayout):
-    def __init__( self, title= ' ', date= ' ', person= ' ', description = '', **kwargs): 
+    def __init__( self, title= "tarefa", date= "12", person= "eu mesmo", description = "ir no mercado", **kwargs): 
+        if not title.strip():
+            title = "Tarefa"
+        if not date.strip():
+            date = "dd/mm/aaaa"
+        if not person.strip():
+            person= "eu mesmo"
+        if not description.strip():
+            description= "vazio"
+
 
         super().__init__(**kwargs)
         self.ids.taskTitle.text= title
-        self.ids.taskDescription.text= date
-        self.ids.tasDate.text= person
-        self.ids.taskPerson.text= description
+        self.ids.taskDate.text= date
+        self.ids.taskPerson.text= person
+        self.ids.taskDescription.text= description
